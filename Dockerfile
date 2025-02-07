3c36abad3e4256b85b8d4dc4c54b65a57b04e093
Carl <junco3@gmail.com>
2025-02-07 15:22:29 -0600

    removed unneeded routes

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.10-alpine
+COPY . /app
+WORKDIR /app
+RUN pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 8080
+
+ENV FLASK_EVN production
+
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:8080", "gsched:create_app()"]
--- gsched/schedule/routes.py
+++ gsched/schedule/routes.py
@@ -8,18 +8,11 @@ registration_bp = Blueprint(
     static_folder='static'
 )
 
-@registration_bp.route('/helloworld')
-def helloworld():
-    return render_template('hello_world.jinja')
 
 @registration_bp.route('/')
 def index():
-    return render_template('schedule.jinja')
-
-@registration_bp.route('/p')
-def pico():
     schedule_data = current_app.config.get("SCHEDULE_DATA")
-    return render_template('example_from_pico.jinja', schedule_data=schedule_data)
+    return render_template('schedule.jinja', schedule_data=schedule_data)
 
 @registration_bp.route('/jsondump')
 def jsondump():
